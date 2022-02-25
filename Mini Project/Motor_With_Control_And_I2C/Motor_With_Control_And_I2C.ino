7e379612638722ab40a7ee4c51341d32ebc6877e
ACurtis0 <100159519+ACurtis0@users.noreply.github.com>
2022-02-24 17:58:16 -0700

    Update Motor_With_Control_And_I2C.ino

--- Mini Project/Motor_With_Control_And_I2C/Motor_With_Control_And_I2C.ino
+++ Mini Project/Motor_With_Control_And_I2C/Motor_With_Control_And_I2C.ino
@@ -2,6 +2,7 @@
 #define ENCODER_OPTIMIZE_INTERRUPTS         // Highly Optimized interrupt in background 
 #define SLAVE_ADDRESS 0x04
 #include "Encoder.h"
+#include "Arduino.h"
 #include "math.h"
 #include <Wire.h>
 
@@ -42,195 +43,129 @@ const unsigned long SAMPLE_TIME = 1000;  // Number of milliseconds between data 
 unsigned long prevSampleTime = 0;        // Time the last data receive occured
 
 void setup() {  
-    // initialize i2c as slave
-    Wire.begin(SLAVE_ADDRESS);
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
 
-    // define callbacks for i2c communication
-    Wire.onReceive(receiveData);
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
     
-    // Begin serial communication
-    Serial.begin(9600);
+  // Begin serial communication
+  Serial.begin(9600);
 
-    // Pin configuration
-    pinMode(ENC_PIN_ONE, INPUT_PULLUP);
-    pinMode(ENC_PIN_TWO, INPUT_PULLUP);
-    pinMode(MOTOR_ENABLE, OUTPUT);
-    pinMode(M1VOLT_SIGN, OUTPUT);
-    pinMode(M2VOLT_SIGN, OUTPUT);
-    pinMode(M1PWM, OUTPUT);
-    pinMode(M2PWM, OUTPUT);
-    pinMode(FAULT_STATUS, INPUT);
+  // Pin configuration
+  pinMode(ENC_PIN_ONE, INPUT_PULLUP);
+  pinMode(ENC_PIN_TWO, INPUT_PULLUP);
+  pinMode(MOTOR_ENABLE, OUTPUT);
+  pinMode(M1VOLT_SIGN, OUTPUT);
+  pinMode(M2VOLT_SIGN, OUTPUT);
+  pinMode(M1PWM, OUTPUT);
+  pinMode(M2PWM, OUTPUT);
+  pinMode(FAULT_STATUS, INPUT);
 }
 
 void loop() {
 
-    // Enable motor
-    digitalWrite(MOTOR_ENABLE, HIGH);
+  // Enable motor
+  digitalWrite(MOTOR_ENABLE, HIGH);
     
-    // Read current encoder value and calculate motor angle.
-    encCount = myEnc.read();
-    currentAngle = accountForRotations(encCount * RAD_PER_COUNT);
-//    Serial.print("Coeff: ");
-//    Serial.println(desiredAngleCoeff);
-//    Serial.print("Curr Ang: ");
-//    Serial.println(currentAngle);
+  // Read current encoder value and calculate motor angle.
+  encCount = myEnc.read();
+  Tc = millis(); // Update current time
+  
+  if(encCount > 3200) encCount -= 3200;
+  currentAngle = encCount * RAD_PER_COUNT;
       
-    while (currentAngle < (desiredAngleCoeff * (PI / 2) - FLOAT_DELTA) || currentAngle > (desiredAngleCoeff * (PI / 2) + FLOAT_DELTA)) {     // While motor 1 is not at desired angle
+  while (currentAngle < (desiredAngleCoeff * (PI / 2) - FLOAT_DELTA) || currentAngle > (desiredAngleCoeff * (PI / 2) + FLOAT_DELTA)) {     // While motor 1 is not at desired angle
 
-        encCount = myEnc.read();
-        currentAngle = accountForRotations(encCount * RAD_PER_COUNT);
-        motor1Speed = control(currentAngle, desiredAngleCoeff * PI/2);
-//        Serial.print("Speed: ");
-//        Serial.println(motor1Speed);
+    encCount = myEnc.read();
+    Tc = millis(); // Update current time
+    
+    if(encCount > 3200) encCount -= 3200;
+    currentAngle = encCount * RAD_PER_COUNT;
+    
+    motor1Speed = control(currentAngle, float(desiredAngleCoeff) * float((PI / 2)) );
 
-        if(motor1Speed < 0) { // If the controller says it needs to go backwards
-            digitalWrite(M1VOLT_SIGN, HIGH); // Set motor direction to CW
-            motor1Speed = abs(motor1Speed); // restore a positive sign
-        }
-        else {
-            digitalWrite(M1VOLT_SIGN, LOW); // Set motor direction to CCW
-        }
-        // Set the motor speed
-        analogWrite(M1PWM, motor1Speed);                                                            // Start rotating motor 1
-
+    if(motor1Speed < 0) { // If the controller says it needs to go backwards
+      digitalWrite(M1VOLT_SIGN, HIGH); // Set motor direction to CW
+      motor1Speed = abs(motor1Speed); // restore a positive sign
     }
-//  analogWrite(M1PWM, 0);    // Stop motor
-//  delay(BRAKE_TIME);        // Give motor a moment to stop        ////// May not need this //////
-
-  // Move motor to desired angle
-  /*switch (desiredAngleCoeff) {
-
-  // Motor should go to 0*PI
-  case 0:
-    while(currentAngle > (0.0 + FLOAT_DELTA)) {   // While motor 1 is not at 0 radians
-      motor1Speed = control(currentAngle, desiredAngleCoeff *PI/2);
-      analogWrite(M1PWM, motor1Speed);         // Start rotating motor 1
-      encCount = myEnc.read();
-      currentAngle = accountForRotations(encCount * RAD_PER_COUNT);
-      ////
-      Serial.println(currentAngle);
-      ////
+    else {
+      digitalWrite(M1VOLT_SIGN, LOW); // Set motor direction to CCW
     }
-    analogWrite(M1PWM, 0);    // Stop motor
-    delay(BRAKE_TIME);        // Give motor a moment to stop        ////// May not need this //////
-                           //                ////
-    Serial.println("Break");
-    //                desiredAngle = 5;
-    //                ////
-    break;
-
-  // Motor should go to PI/2
-  case 1:
-    while(currentAngle < ((PI / 2) - FLOAT_DELTA) || currentAngle > ((PI / 2) + FLOAT_DELTA)) {     // While motor 1 is not at PI/2 radians
-      motor1Speed = control(currentAngle, desiredAngleCoeff *PI/2);
-      analogWrite(M1PWM, motor1Speed);                                                            // Start rotating motor 1
-      encCount = myEnc.read();
-      currentAngle = accountForRotations(encCount * RAD_PER_COUNT);
-    }
-    analogWrite(M1PWM, 0);    // Stop motor
-    delay(BRAKE_TIME);        // Give motor a moment to stop        ////// May not need this //////
-    break;
-
-  // Motor should go to PI
-  case 2:
-    while(currentAngle < (PI - FLOAT_DELTA) || currentAngle > (PI + FLOAT_DELTA)) {     // While motor 1 is not at PI radians
-      motor1Speed = control(currentAngle, desiredAngleCoeff *PI/2);
-      analogWrite(M1PWM, motor1Speed);                                                // Start rotating motor 1
-      encCount = myEnc.read();
-      currentAngle = accountForRotations(encCount * RAD_PER_COUNT);
-    }
-    analogWrite(M1PWM, 0);    // Stop motor
-    delay(BRAKE_TIME);        // Give motor a moment to stop        ////// May not need this //////
-    break;
-
-  // Motor should go to 3PI/2
-  case 3:
-    while(currentAngle < ((3 * PI/2) - FLOAT_DELTA) || currentAngle > ((3 * PI/2) + FLOAT_DELTA)) {     // While motor 1 is not at 3PI/2 radians
-      motor1Speed = control(currentAngle, desiredAngleCoeff *PI/2);
-      analogWrite(M1PWM, motor1Speed);                                                                // Start rotating motor 1
-      encCount = myEnc.read();
-      currentAngle = accountForRotations(encCount * RAD_PER_COUNT);
-    }
-    analogWrite(M1PWM, 0);    // Stop motor
-    delay(BRAKE_TIME);        // Give motor a moment to stop        ////// May not need this //////
-    break;
-
-  // If desiredAngle is not 0-3, do nothing
-  default:
-    break;
+    // Set the motor speed
+    analogWrite(M1PWM, motor1Speed);                                                            // Start rotating motor 1
   }
-  //        angleRead = false;   // Reset data flag
-  //    }*/
+  
 }
 
 int control(float current, float desired) {
-    float I = 0; 
-    float D = 0;
+  float I = 0; 
+  float D = 0;
     
-    int cOutput, maxOutput = 255;   // or 400 if using the motorShield header
-    Tc = millis();                  // Update current time
-    error = current - desired;      // Update error
-    Serial.println(error);
+  int cOutput, maxOutput = 255;   // or 400 if using the motorShield header
+  
+  error = current - desired;      // Update error
+  Serial.println(error);
 
-    if (Ts > 0) {
-        // Calculate D
-        D = (error - pastError) / float(Ts);
-        pastError = error;
-    } 
+  if (Ts > 0) {
+    // Calculate D
+    D = (error - pastError) / float(Ts);
+    pastError = error;
+  } 
+  else {
+    D = 0;
+  }
+
+  // Calculate I
+  I += float(Ts) * error;
+
+  // Calculate controller output
+  cOutput = KP * error + KI * I + KD * D;
+
+  // If calculated controller output is higher than the maximum output,
+  // set output to the maximum output with matching sign
+  if (abs(cOutput) > maxOutput) {
+    if (cOutput < 0) {
+      cOutput = -1 * maxOutput;
+    }
     else {
-        D = 0;
+      cOutput = maxOutput;
     }
+  }
 
-    // Calculate I
-    I += float(Ts) * error;
+  // Set Ts
+  Ts = millis() - Tc;
+  Tc = millis();
 
-    // Calculate controller output
-    cOutput = KP * error + KI * I + KD * D;
-
-    // If calculated controller output is higher than the maximum output,
-    // set output to the maximum output with matching sign
-    if (abs(cOutput) > maxOutput) {
-        if (cOutput < 0) {
-            cOutput = -1 * maxOutput;
-        }
-        else {
-            cOutput = maxOutput;
-        }
-    }
-
-    // Set Ts
-    Ts = millis() - Tc;
-    Tc = millis();
-
-    //TODO scale output to [-255, 255]
-    cOutput = cOutput * 5;
-    Serial.print("cOutput: ");
-    Serial.println(cOutput);
-    return cOutput;
+  //TODO scale output to [-255, 255]
+  cOutput = cOutput * 5;
+  Serial.print("cOutput: ");
+  Serial.println(cOutput);
+  return cOutput;
 }
 
 // Adjust current angle to be between 0 and 2PI radians by accounting for multiple rotations
 float accountForRotations(float currentAngle) {
-    if (currentAngle > 2 * PI ) {
-        int rotations = int(currentAngle / (2 * PI));
-        currentAngle -= rotations * 2 * PI;
-    }
-    return currentAngle;  
+  if (currentAngle > 2 * PI ) {
+    int rotations = int(currentAngle / (2 * PI));
+    currentAngle -= rotations * 2 * PI;
+  }
+  return currentAngle;  
 }
 
 // callback for received data
 void receiveData(int byteCount) { 
-    // While there are still bytes to read, read them and store the most recent to number
-    while (Wire.available()) {
-        int nextAngleCoeff = Wire.read(); 
-        unsigned long currSampleTime = millis();
-        // If time since previous sample exceeds SAMPLE_TIME receive a new input
-        if (currSampleTime - prevSampleTime > SAMPLE_TIME) {
-            desiredAngleCoeff = nextAngleCoeff;
-            angleRead = true;
-            prevSampleTime = currSampleTime;
-            Serial.print("Desired Angle: ");       
-            Serial.println(desiredAngleCoeff);
-        }
-    }  
+  // While there are still bytes to read, read them and store the most recent to number
+  while (Wire.available()) {
+    int nextAngleCoeff = Wire.read(); 
+    unsigned long currSampleTime = millis();
+    // If time since previous sample exceeds SAMPLE_TIME receive a new input
+    if (currSampleTime - prevSampleTime > SAMPLE_TIME) {
+      desiredAngleCoeff = nextAngleCoeff;
+      angleRead = true;
+      prevSampleTime = currSampleTime;
+      Serial.print("Desired Angle: ");       
+      Serial.println(desiredAngleCoeff);
+    }
+  }  
 }
